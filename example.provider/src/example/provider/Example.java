19a544020c64d8f1b1c88555c148eef35753b984
Jon Passki <jon@passki.us>
2017-12-25 08:18:12 -0800

    Post-default project creation

---
+++ example.provider/src/example/provider/Example.java
@@ -0,0 +1,10 @@
+package example.provider;
+
+import org.osgi.service.component.annotations.*;
+
+@Component
+public class Example {
+
+	// TODO: class provided by template
+
+}
---
+++ example.provider/test/example/provider/ExampleTest.java
@@ -0,0 +1,14 @@
+package example.provider;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ExampleTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
